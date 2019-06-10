c749bd34790aaeae2cba2a956134e9faa71ee492
Le H Ngo <seiji@o2.pl>
2019-06-10 15:07:22 +0100

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,20 @@
 class FundingRound
 
+    attr_reader(:startup, :venture, :type, :amount)
+
+    @@all = []
+
+    def initialize(venture, startup, type, amount)
+        
+        @venture = venture
+        @startup = startup
+        @type  = type
+        @amount = amount.to_f
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
